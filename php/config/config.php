8fb64a25b76b7e7fb2cbfc0528b11102bf06b080
Srijagatheeswaran Raja <srijagatheeswaran.raja@dci.in>
2025-07-13 14:00:21 +0530

    chenges in config for deploy

--- php/config/config.php
+++ php/config/config.php
@@ -4,7 +4,7 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 function loadEnv()
 {
-    $dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__, 2));
+    $dotenv = Dotenv\Dotenv::createImmutable('/var/www/html');
     $dotenv->load();
 }
 
