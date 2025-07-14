03e96ea8be44dd139dc29ca9ac3c29c5945c78f6
Srijagatheeswaran Raja <srijagatheeswaran.raja@dci.in>
2025-07-14 21:40:21 +0530

    fix install env 1

--- php/config/config.php
+++ php/config/config.php
@@ -1,11 +1,26 @@
 <?php
 
+// require_once __DIR__ . '/../../vendor/autoload.php';
+
+
+// function loadEnv()
+// {
+//     $dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__, 2));
+//     $dotenv->load();
+// }
+
 require_once __DIR__ . '/../../vendor/autoload.php';
 
+use Dotenv\Dotenv;
 
 function loadEnv()
 {
-    $dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__, 2));
-    $dotenv->load();
+    $envPath = dirname(__DIR__, 2);
+
+    if (file_exists($envPath . '/.env')) {
+        $dotenv = Dotenv::createImmutable($envPath);
+        $dotenv->load();
+    }
 }
+
 
